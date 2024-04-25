d23b5060d61591e70410991864f29c5cc7039c15
nimaCod <t.nima.tajik@gmail.com>
2024-04-25 16:56:48 +0330

    Update on client and server messageing (Full Duplex)

--- CA02 - Concurrent ticket reservation system/main.go
+++ CA02 - Concurrent ticket reservation system/main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	GetListEvents = 0
+	ReserveEvent  = 1
+)
+
 type Event struct {
 	ID               string
 	Name             string
@@ -23,10 +28,17 @@ type Ticket struct {
 	EventId string
 }
 
-// Object to send Resesrve Request through channel
-type ReserveRequest struct {
+// Object to send Resesrve Request and get event list through channel
+type UserRequest struct {
+	Action      int // Action = 0 to get list of events and 1 to reserve
 	EventId     string
 	TicketCount int
+	responses   chan ServerResponse
+}
+
+type ServerResponse struct {
+	message   string
+	eventList []Event
 }
 
 // Struct that Maps event to their ID for searching
@@ -45,12 +57,18 @@ func generateUUID() string {
 }
 
 // Store Event in eventList
-func (e *EventList) Store(id string, event *Event) {
+func (e *EventList) Store(id string, event *Event) error {
 	_, ok := e.eventsList[id]
 	if !ok {
 		e.eventsList[id] = *event
 		e.count++
-	} // TODO do we need to handle the else???? if id already exists?
+		log.Println("Stored New Event:", event)
+		log.Println("Count events:", e.count)
+		return nil
+	} else { // TODO do we need to handle the else???? if id already exists?
+		log.Println("Event id already Exists!") // TODO: Does this ever happen?
+		return fmt.Errorf("Event id already Exists!")
+	}
 }
 
 // Find event From event list
@@ -67,8 +85,8 @@ func (ts *TicketService) CreateEvent(name string, date time.Time, totalTickets i
 		TotalTickets:     totalTickets,
 		AvailableTickets: totalTickets,
 	}
-	ts.activeEvents.Store(event.ID, event)
-	return event, nil
+	ok := ts.activeEvents.Store(event.ID, event)
+	return event, ok
 }
 
 func (ts *TicketService) ListEvents() []Event {
@@ -84,19 +102,17 @@ func (el *EventList) decreaseAvailableTicket(id string, count int) {
 	temp := el.eventsList[id]
 	temp.AvailableTickets -= count
 	el.eventsList[id] = temp
+	log.Println("Decresed available tickets for ID: ", id, " count=", count, " available tickets after =", el.eventsList[id].AvailableTickets)
 }
 
 func (ts *TicketService) BookTickets(eventID string, numTickets int) ([]string, error) {
 	// Implement concurrency control here (Step 3)
-	fmt.Println("id:", eventID)
-	event, ok := ts.activeEvents.Load(eventID)
+	ev, ok := ts.activeEvents.Load(eventID)
 	if !ok {
 		return nil, fmt.Errorf("event not found")
 	}
 
-	// ev := event.(*Event)
-	ev := event
-	if ev.AvailableTickets < numTickets {
+	if ev.AvailableTickets < numTickets { // TODO: Possible race condition scenario
 		return nil, fmt.Errorf("not enough tickets available")
 	}
 
@@ -106,73 +122,96 @@ func (ts *TicketService) BookTickets(eventID string, numTickets int) ([]string, 
 		ticketIDs = append(ticketIDs, ticketID)
 		ev.ReservedTickets = append(ev.ReservedTickets, ticketID)
 	}
-	log.Println("ticket count ", event.AvailableTickets)
-
 	ts.activeEvents.decreaseAvailableTicket(eventID, numTickets)
-	log.Println("ticket count ", event.AvailableTickets)
-
-	ev.AvailableTickets -= numTickets
-	ts.activeEvents.Store(eventID, &ev)
+	// ts.activeEvents.Store(eventID, &ev)
 	return ticketIDs, nil
 }
 
-// server
-func (ts *TicketService) receiveReserveRequest(requestChannel <-chan ReserveRequest, wg *sync.WaitGroup) {
+// for each client request we create a thread and run this function that handles the request and sends the response
+func (ts *TicketService) handleReceiveUserRequest(req UserRequest, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for req := range requestChannel {
-		tickets, err := ts.BookTickets(req.EventId, req.TicketCount)
-		log.Println("ticket", tickets, err)
+	var serverResponse ServerResponse
+	if req.Action == GetListEvents { // TODO: Implement caching mechanism
+		serverResponse.message = "List of available events"
+		serverResponse.eventList = ts.ListEvents()
+		log.Println("Prepared list of events no cache")
+	} else {
+		tickets, err := ts.BookTickets(req.EventId, req.TicketCount) // TODO put this part in a go routine and continue to next request
+		if err != nil {
+			serverResponse.message = err.Error()
+			log.Println(err.Error())
+		} else {
+			serverResponse.message = "Reserved succesfuly"
+			log.Println("ticket Reserved: ", tickets, err)
+		}
 	}
+	req.responses <- serverResponse
+	close(req.responses)
+}
+
+// server
+func (ts *TicketService) receiveUserRequest(requestChannel <-chan UserRequest, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var waitGroup sync.WaitGroup
+	for req := range requestChannel {
+		waitGroup.Add(1)
+		go ts.handleReceiveUserRequest(req, &waitGroup)
+	}
+	waitGroup.Wait()
+}
+
+
+// in this function we can create one or mutliple server codes to handle clients
+func createServer(wg *sync.WaitGroup, channel chan UserRequest, ticketService *TicketService) {
+	wg.Add(1)
+	go ticketService.receiveUserRequest(channel, wg)
 }
 
 // client
-func sendReserveRequest(req ReserveRequest, requestChannel chan<- ReserveRequest) {
+func sendUserRequest(req UserRequest, requestChannel chan<- UserRequest) {
 	requestChannel <- req
 	close(requestChannel)
+}
 
+// This function creates clinets
+// TODO add client interface
+func createClinet(channel chan UserRequest, event *Event) {
+	var responseChannel = make(chan ServerResponse)
+	req := UserRequest{Action: ReserveEvent, EventId: event.ID, TicketCount: 5, responses: responseChannel}
+	go sendUserRequest(req, channel)
+	for req := range responseChannel {
+		log.Println("Got Response from server: ", req)
+	}
+}
+
+// In this function we create events that clients will reserve
+func createEvents(ticketService *TicketService) *Event {
+	event, err := ticketService.CreateEvent("event0", time.Now(), 100)
+	if err != nil {
+		log.Println(err)
+	}
+	return event // TODO: there would be no return
 }
 
 // creates ticket service and initializes the eventlist of it
-func initticketService() *TicketService {
+func initTicketService() *TicketService {
 	var ticketService = new(TicketService)
 	ticketService.activeEvents.eventsList = make(map[string]Event)
 	return ticketService
 }
 
-func createEvents(ticketService *TicketService) *Event {
-	event, err := ticketService.CreateEvent("event0", time.Now(), 100)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Created new Event: ", event)
-	}
-	return event // TODO: there would be no return
-}
-
-func createServer(wg *sync.WaitGroup,channel chan ReserveRequest,ticketService *TicketService){
-	wg.Add(1)
-	go ticketService.receiveReserveRequest(channel, wg)
-
-}
-
-func createClinet(channel chan ReserveRequest,event *Event){ 
-	req := ReserveRequest{EventId: event.ID, TicketCount: 5}
-	go sendReserveRequest(req, channel)
-
-}
-
 func main() {
 	log.Println("Program Started !")
 	var waitGroup sync.WaitGroup
-	var ticketService = initticketService()
+	var ticketService = initTicketService()
 	var event = createEvents(ticketService) // TODO must avoid return value here
 
-	channel := make(chan ReserveRequest, 100)
+	channel := make(chan UserRequest)
 
-	createServer(&waitGroup,channel,ticketService)
-	createClinet(channel,event) // TODO Must not use event as argument
+	createServer(&waitGroup, channel, ticketService)
+	createClinet(channel, event) // TODO Must not use event as argument
 
 	waitGroup.Wait()
-	log.Println(" sfdfd", ticketService.activeEvents.eventsList)
+	log.Println("Event after reserve", ticketService.activeEvents.eventsList)
 	log.Println("Program Finished!")
 }
